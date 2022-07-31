72d31f459d696ddb3f6836bb2e1c2a75f6bd9275
Dev P Ishaili <devpishaili@gmail.com>
2022-07-31 10:25:39 +0100

    Updated

--- image-editor/script.js
+++ image-editor/script.js
@@ -3,16 +3,55 @@ const fileInput = document.querySelector(".file-input"),
   filterName = document.querySelector(".filter-info .name"),
   filterValue = document.querySelector(".filter-info .value"),
   filterSlider = document.querySelector(".slider input"),
+  rotateOptions = document.querySelectorAll(".rotate button"),
   previewImg = document.querySelector(".preview-img img"),
-  chooseImgBtn = document.querySelector(".choose-img");
+  resetFilterBtn = document.querySelector(".reset-filter"),
+  chooseImgBtn = document.querySelector(".choose-img"),
+  saveImgBtn = document.querySelector(".save-img");
 
 let brightness = 100,
   saturation = 100,
   inversion = 0,
   grayscale = 0;
 
+let rotate = 0,
+  flipHorizontal = 1,
+  flipVertical = 1;
+
 const applyFilters = () => {
+  previewImg.style.transform = `rotate(${rotate}deg) scale(${flipHorizontal}, ${flipVertical})`;
   previewImg.style.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) grayscale(${grayscale}%)`;
+};
+
+const updateFilter = () => {
+  filterValue.innerText = `${filterSlider.value}%`;
+  const selectedFilter = document.querySelector(".filter .active");
+
+  if (selectedFilter.id === "brightness") {
+    brightness = filterSlider.value;
+  } else if (selectedFilter.id === "saturation") {
+    saturation = filterSlider.value;
+  } else if (selectedFilter.id === "inversion") {
+    inversion = filterSlider.value;
+  } else {
+    grayscale = filterSlider.value;
+  }
+
+  applyFilters();
+};
+
+const resetFilter = () => {
+  // reseting all variable values to their default value
+  brightness = 100;
+  saturation = 100;
+  inversion = 0;
+  grayscale = 0;
+  rotate = 0;
+  flipHorizontal = 1;
+  flipVertical = 1;
+
+  filterOptions[0].click();
+  applyFilters();
 };
 
 const loadImage = () => {
@@ -20,8 +59,36 @@ const loadImage = () => {
   if (!file) return; // return if user hasn't selected file on file dialog open
   previewImg.src = URL.createObjectURL(file);
   previewImg.addEventListener("load", () => {
+    resetFilterBtn.click(); // clicking reset btn, so filter value resets, if the user select new img.
     document.querySelector(".container").classList.remove("disable");
   });
+};
+
+const saveImage = () => {
+  const canvas = document.createElement("canvas");
+  const ctx = canvas.getContext("2d");
+  canvas.width = previewImg.naturalWidth;
+  canvas.height = previewImg.naturalHeight;
+
+  // applying user selected filters to canvas filter
+  ctx.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) grayscale(${grayscale}%)`; // translating canvas from center
+  if (rotate !== 0) {
+    // if rotate value isn't 0, rotate the canvas
+    ctx.rotate((rotate * Math.PI) / 180);
+  }
+  ctx.translate(canvas.width / 2, canvas.height / 2);
+  ctx.scale(flipHorizontal, flipVertical);
+  ctx.drawImage(
+    previewImg,
+    -canvas.width / 2,
+    -canvas.height / 2,
+    canvas.width,
+    canvas.height
+  );
+  const link = document.createElement("a"); // creating <a> element;
+  link.download = "image.jpg"; // passing <a> tag download value to 'image.jpg'
+  link.href = canvas.toDataURL(); // passing <a> tag href to canvas data url
+  link.click(); // clicking <a> tag so the image download
 };
 
 filterOptions.forEach((option) => {
@@ -50,23 +117,25 @@ filterOptions.forEach((option) => {
   });
 });
 
-const updateFilter = () => {
-  filterValue.innerText = `${filterSlider.value}%`;
-  const selectedFilter = document.querySelector(".filter .active");
+rotateOptions.forEach((option) => {
+  option.addEventListener("click", () => {
+    if (option.id === "left") {
+      rotate -= 90;
+      console.log("right");
+    } else if (option.id === "right") {
+      rotate += 90;
+    } else if (option.id === "horizontal") {
+      flipHorizontal = flipHorizontal === 1 ? -1 : 1;
+    } else {
+      flipVertical = flipVertical === 1 ? -1 : 1;
+    }
 
-  if (selectedFilter.id === "brightness") {
-    brightness = filterSlider.value;
-  } else if (selectedFilter.id === "saturation") {
-    saturation = filterSlider.value;
-  } else if (selectedFilter.id === "inversion") {
-    inversion = filterSlider.value;
-  } else {
-    grayscale = filterSlider.value;
-  }
-
-  applyFilters();
-};
+    applyFilters();
+  });
+});
 
 fileInput.addEventListener("change", loadImage);
 filterSlider.addEventListener("input", updateFilter);
+resetFilterBtn.addEventListener("click", resetFilter);
+saveImgBtn.addEventListener("click", saveImage);
 chooseImgBtn.addEventListener("click", () => fileInput.click());
